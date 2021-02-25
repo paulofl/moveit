ccd490080ceb85fad89cfe6b2fc9c4665fd28807
paulofl <paulofernandocornelio@gmail.com>
2021-02-25 19:43:41 -0300

    dia-4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/global.css';
 
+import React from 'react';
 import { ChanllengesProvider } from '../contexts/ChallengesContext'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChanllengesProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </ChanllengesProvider>
   )
 }
